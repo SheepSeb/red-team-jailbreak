36c4612a7ff7cfc98959974b2f755f026a644128
Vlad Magon <109043350+Fr0nty@users.noreply.github.com>
2024-04-22 11:03:32 +0300

    Add files via upload

---
+++ tests/response_multimodal.py
@@ -0,0 +1,24 @@
+import unittest
+
+from response_api import ResponseAPI
+
+
+class TestResponseAPI(unittest.TestCase):
+    def test_get_response_text(self):
+        response_api = ResponseAPI()
+        response = response_api.get_response_multimodal('llava', 'What is in this picture?', ["iVBORw0KGgoAAAANSUhEUgAAAG0AAABmCAYAAADBPx+VAAAACXBIWXMAAAsTAAALEwEAmpwYAAAAAXNSR0IArs4c6QAAAARnQU1BAACxjwv8YQUAAA3VSURBVHgB7Z27r0zdG8fX743i1bi1ikMoFMQloXRpKFFIqI7LH4BEQ+NWIkjQuSWCRIEoULk0gsK1kCBI0IhrQVT7tz/7zZo888yz1r7MnDl7z5xvsjkzs2fP3uu71nNfa7lkAsm7"])
+        self.assertNotEqual(response, '')
+
+    def test_is_str(self):
+        response_api = ResponseAPI()
+        response = response_api.get_response_multimodal('llava', 'What is in this picture?', ["iVBORw0KGgoAAAANSUhEUgAAAG0AAABmCAYAAADBPx+VAAAACXBIWXMAAAsTAAALEwEAmpwYAAAAAXNSR0IArs4c6QAAAARnQU1BAACxjwv8YQUAAA3VSURBVHgB7Z27r0zdG8fX743i1bi1ikMoFMQloXRpKFFIqI7LH4BEQ+NWIkjQuSWCRIEoULk0gsK1kCBI0IhrQVT7tz/7zZo888yz1r7MnDl7z5xvsjkzs2fP3uu71nNfa7lkAsm7"])
+        self.assertIsInstance(response, str)
+
+    def test_newline(self):
+        response_api = ResponseAPI()
+        response = response_api.get_response_multimodal('llava', 'What is in this picture?', ["iVBORw0KGgoAAAANSUhEUgAAAG0AAABmCAYAAADBPx+VAAAACXBIWXMAAAsTAAALEwEAmpwYAAAAAXNSR0IArs4c6QAAAARnQU1BAACxjwv8YQUAAA3VSURBVHgB7Z27r0zdG8fX743i1bi1ikMoFMQloXRpKFFIqI7LH4BEQ+NWIkjQuSWCRIEoULk0gsK1kCBI0IhrQVT7tz/7zZo888yz1r7MnDl7z5xvsjkzs2fP3uu71nNfa7lkAsm7"])
+        self.assertNotIn('\n', response)
+
+
+if __name__ == '__main__':
+    unittest.main()
