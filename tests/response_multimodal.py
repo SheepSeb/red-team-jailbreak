c5aab5f7737988426657fa2b31520f1db265b0db
Vlad Magon <109043350+Fr0nty@users.noreply.github.com>
2024-05-03 18:13:16 +0300

    Update response_multimodal.py

--- tests/response_multimodal.py
+++ tests/response_multimodal.py
@@ -1,25 +1,28 @@
 import unittest
 import base64
-from src.mm_response_api import mm_ResponseAPI
+from src.mm_response_api import MM_ResponseAPI
 
-with open('test.jpg', 'rb') as f:
+
+    
+class MM_TestResponseAPI(unittest.TestCase):
+
+    with open('test.jpg', 'rb') as f:
     image_data = base64.b64encode(f.read()).decode('utf-8')
     
-class mm_TestResponseAPI(unittest.TestCase):
     def test_get_response_with_image(self):
-        response_api = mm_ResponseAPI()
+        response_api = MM_ResponseAPI()
 
         response = response_api.get_response('llava', 'What is in the image?', image_data)
         self.assertNotEqual(response, '')
 
     def test_get_response_with_image_is_str(self):
-        response_api = mm_ResponseAPI()
+        response_api = MM_ResponseAPI()
 
         response = response_api.get_response('llava', 'What is in the image?', image_data)
         self.assertIsInstance(response, str)
 
     def test_get_response_with_image_no_newline(self):
-        response_api = mm_ResponseAPI()
+        response_api = MM_ResponseAPI()
 
         response = response_api.get_response('llava', 'What is in the image?', image_data)
         self.assertNotIn('\n', response)
