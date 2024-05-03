6bde6ba02453250ef3c9c2588676785b44574d7d
Fr0nty <v_pain69@yahoo.ro>
2024-05-03 19:57:56 +0300

    Added the global path to the multimodal response API and the test for it

--- tests/response_multimodal.py
+++ tests/response_multimodal.py
@@ -1,30 +1,32 @@
 import unittest
 import base64
-from src.mm_response_api import MM_ResponseAPI
+from src.mm_response_api import MMResponseAPI
 
 
     
-class MM_TestResponseAPI(unittest.TestCase):
+class TestMMResponseAPI(unittest.TestCase):
 
-    with open('test_images/test.jpg', 'rb') as f:
-    image_data = base64.b64encode(f.read()).decode('utf-8')
+    
     
     def test_get_response_with_image(self):
-        response_api = MM_ResponseAPI()
-
-        response = response_api.get_response_multimodal('llava', 'What is in the image?', image_data)
+        response_api = MMResponseAPI()
+        image_path = 'test_images/test.jpg'  # Relative path to the image
+        image = self.get_image_data(image_path)
+        response = response_api.get_response_multimodal('llava', 'What is in the image?', image)
         self.assertNotEqual(response, '')
 
     def test_get_response_with_image_is_str(self):
-        response_api = MM_ResponseAPI()
-
-        response = response_api.get_response_multimodal('llava', 'What is in the image?', image_data)
+        response_api = MMResponseAPI()
+        image_path = 'test_images/test.jpg'  # Relative path to the image
+        image = self.get_image_data(image_path)
+        response = response_api.get_response_multimodal('llava', 'What is in the image?', image)
         self.assertIsInstance(response, str)
 
     def test_get_response_with_image_no_newline(self):
-        response_api = MM_ResponseAPI()
-
-        response = response_api.get_response_multimodal('llava', 'What is in the image?', image_data)
+        response_api = MMResponseAPI()
+        image_path = 'test_images/test.jpg'  # Relative path to the image
+        image = self.get_image_data(image_path)
+        response = response_api.get_response_multimodal('llava', 'What is in the image?', image)
         self.assertNotIn('\n', response)
 
 if __name__ == '__main__':
