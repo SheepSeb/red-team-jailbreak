3731c139be45c915a3d51f4f08a9e8ee59d5460f
Sebastian Nae <naesebi2000@gmail.com>
2024-05-04 10:28:33 +0300

    Parent class for mm_response, static method for get image data, corrected tests

--- src/mm_response_api.py
+++ src/mm_response_api.py
@@ -1,21 +1,22 @@
 import requests
-import json
+from src.response_api import ResponseAPI
 import base64
 import os
 
-class MMResponseAPI:
+
+class MMResponseAPI(ResponseAPI):
 
     def __init__(self):
-        self.url = 'http://localhost:11434/api/generate'
-        
-    def get_image_data(self, image_path):
+        super().__init__()
+
+    @staticmethod
+    def get_image_data(image_path):
         if not os.path.isabs(image_path):
             image_path = os.path.abspath(image_path)
 
         with open(image_path, 'rb') as f:
             image_data = base64.b64encode(f.read()).decode('utf-8')
         return image_data
-
 
     def get_response_multimodal(self, model: str, prompt: str, image: str) -> str:
 
@@ -31,18 +32,3 @@ class MMResponseAPI:
             return self.parse_response(response)
         else:
             return f"Request failed with status code: {response.status_code}"
-
-    @staticmethod
-    def parse_response(response: requests.Response) -> str:
-        full_response = ""
-        for item in response.iter_lines():
-            json_response = item.decode('utf-8')
-            if json_response.strip():  # skip empty lines
-                response_data = json.loads(json_response)
-                full_response += response_data.get('response', '')
-
-        # Remove any trailing whitespace and newline characters
-        full_response = full_response.strip()
-        full_response = full_response.replace('\n', ' ')
-
-        return full_response
