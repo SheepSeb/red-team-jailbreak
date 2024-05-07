7cf31569cf070bd5b20854d3026da8516e938927
Sebastian Nae <naesebi2000@gmail.com>
2024-05-07 17:00:32 +0300

    added starter for persona

---
+++ src/persona/main.py
@@ -0,0 +1,18 @@
+import torch
+from src.response_api import ResponseAPI
+from src.mm_response_api import MMResponseAPI
+
+ollama_api_text = ResponseAPI()
+ollama_api_mm = MMResponseAPI()
+
+judge_llm = ""
+def text_only():
+    pass
+
+
+def multimodal():
+    pass
+
+
+if __name__ == '__main__':
+    pass
